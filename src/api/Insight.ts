344d5c3452477b5cc8acacdb95a9f963fc1f2ede
MrMirror21 <49388937+MrMirror21@users.noreply.github.com>
2024-03-15 23:09:01 +0900

    [feat] #1 - 제목 추출 기능 추가 및 각 데이터 가공 처리

--- src/api/Insight.ts
+++ src/api/Insight.ts
@@ -18,7 +18,7 @@ const fetchSummary = async (link: string) => {
     data: {
       model: 'gpt-4',
       messages: [
-        { role: 'system', content: 'You are helpful assistant. Your job is summarize the article accessible through the provided link in one sentence in Korean, and extract three keywords that can be used for classification purposes.' },
+        { role: 'system', content: 'You are helpful assistant. Your job is Get title of article, and summarize the article accessible through the provided link in one sentence in Korean, and extract three keywords that can be used for classification purposes.' },
         { role: 'user', content: link },
       ],
       max_tokens: 5000,
@@ -31,7 +31,10 @@ const fetchSummary = async (link: string) => {
 
 export const useGetSummary = (link: string) => {
   const { isLoading, error, data } = useQuery({queryKey: ["get-summary"], queryFn: () => fetchSummary(link), enabled: !!link});
-  const summary = data?.choices?.[0]?.message.content;
-
-  return { isLoading, error, summary };
+  const result = {
+    title: data?.choices?.[0]?.message.content.split("요약:")[0].split("제목:")[1],
+    summary: data?.choices?.[0]?.message.content.split("요약:")[1].split("키워드:")[0],
+    keywords: data?.choices?.[0]?.message.content.split("키워드:")[1].split(",")
+  }
+  return { isLoading, error, result };
 };
