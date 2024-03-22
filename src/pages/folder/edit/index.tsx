256881569329cdb279332f1e280d187bb70eae28
MrMirror21 <49388937+MrMirror21@users.noreply.github.com>
2024-03-22 19:27:07 +0900

    [Add] #21 - 폴더 편집하기 화면 UI 추가

---
+++ src/pages/folder/edit/index.tsx
@@ -0,0 +1,240 @@
+import Header from '@/components/common/Header';
+import { NextPage } from 'next';
+import styled from 'styled-components';
+import SearchIcon from '@svg/search-icon.svg';
+import { FolderList } from '@/constants/FolderList';
+import { colorDecoder } from '@/utils/folder';
+import { useState } from 'react';
+import { Folder } from '@/types/folder';
+
+interface Props {}
+
+const FolderEdit: NextPage<Props> = ({}) => {
+  const [searchInput, setSearchInput] = useState('');
+  const [newFolderList, setNewFolderList] = useState<Folder[]>(FolderList);
+  const [searchedFolderList, setSearchedFolderList] = useState<Folder[]>([]);
+  const folderNameList = newFolderList.map((folder) => folder.folderName);
+  const [isEditingFolder, setIsEditingFolder] = useState('');
+  const [newFolderName, setNewFolderName] = useState('');
+
+  const handleSearch = (value: string) => {
+    setSearchInput(value);
+    setSearchedFolderList(
+      newFolderList.filter((folder) => folder.folderName.includes(value)),
+    );
+  };
+
+  const handleBlur = () => {
+    setNewFolderName('');
+    setIsEditingFolder('');
+  };
+
+  const checkEnter = (key: string, folder: Folder) => {
+    if (key === 'Enter') {
+      if (folderNameList.includes(newFolderName)) {
+        alert('이미 같은 이름의 폴더가 존재합니다!');
+        handleBlur();
+        return;
+      }
+      const newList = newFolderList;
+      newList[newFolderList.indexOf(folder)] = {
+        ...folder,
+        folderName: newFolderName,
+      };
+      setNewFolderList(newList);
+      setNewFolderName('');
+      setIsEditingFolder('');
+    }
+  };
+
+  const handleModalOn = (isOn: boolean, folder: Folder) => {};
+  const saveFolder = () => {};
+  return (
+    <>
+      <Wrapper>
+        <Header title="폴더 편집하기" />
+        <span className="link save" onClick={() => saveFolder()}>
+          저장
+        </span>
+        <SearchSection>
+          <SearchIcon />
+          <SearchInput
+            value={searchInput}
+            onChange={(e) => handleSearch(e.target.value)}
+            placeholder="폴더 검색"
+          />
+        </SearchSection>
+        <FolderSection>
+          {searchInput === '' &&
+            newFolderList.map((folder, idx) => (
+              <FolderRow key={idx}>
+                {colorDecoder(folder.folderColor, 'small')}
+                <div className="text-container">
+                  {isEditingFolder === folder.folderName ? (
+                    <Input
+                      type="text"
+                      value={newFolderName}
+                      onChange={(e) => setNewFolderName(e.target.value)}
+                      onKeyDownCapture={(e) => checkEnter(e.key, folder)}
+                      onBlur={handleBlur}
+                      placeholder={folder.folderName}
+                    />
+                  ) : (
+                    <span
+                      className="name text"
+                      onClick={() => setIsEditingFolder(folder.folderName)}
+                    >
+                      {folder.folderName}
+                    </span>
+                  )}
+                  <span
+                    className="edit text"
+                    onClick={() => handleModalOn(true, folder)}
+                  >
+                    편집
+                  </span>
+                </div>
+              </FolderRow>
+            ))}
+          {searchInput !== '' &&
+            searchedFolderList.map((folder, idx) => (
+              <FolderRow key={idx}>
+                {colorDecoder(folder.folderColor, 'small')}
+                <div className="text-container">
+                  {isEditingFolder === folder.folderName ? (
+                    <Input
+                      type="text"
+                      value={newFolderName}
+                      onChange={(e) => setNewFolderName(e.target.value)}
+                      onKeyDownCapture={(e) => checkEnter(e.key, folder)}
+                      onBlur={handleBlur}
+                      placeholder={folder.folderName}
+                    />
+                  ) : (
+                    <span
+                      className="name text"
+                      onClick={() => setIsEditingFolder(folder.folderName)}
+                    >
+                      {folder.folderName}
+                    </span>
+                  )}
+                  <span
+                    className="edit text"
+                    onClick={() => handleModalOn(true, folder)}
+                  >
+                    편집
+                  </span>
+                </div>
+              </FolderRow>
+            ))}
+        </FolderSection>
+      </Wrapper>
+    </>
+  );
+};
+
+export default FolderEdit;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  position: relative;
+  justify-content: center;
+  .title {
+    color: #1f1f1f;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 27.146px; /* 135.732% */
+    height: 28px;
+  }
+  .link {
+    color: var(--Primary-500, #3184ff);
+    text-align: right;
+    /* Body-16-SB */
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 140%; /* 22.4px */
+  }
+  .save {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+  }
+`;
+
+const SearchSection = styled.div`
+  display: flex;
+  width: calc(100% - 40px);
+  margin: 20px auto;
+  padding: 10px 16px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 8px;
+  background: #f4f5f7;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  border: none;
+  outline: none;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 21px; /* 131.25% */
+  letter-spacing: -0.32px;
+  background: #f4f5f7;
+`;
+
+const FolderSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 17px;
+`;
+
+const FolderRow = styled.div`
+  display: flex;
+  margin: auto;
+  flex-direction: row;
+  width: calc(100% - 40px);
+  .text-container {
+    width: calc(100% - 100px - 40px);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-left: 28px;
+  }
+  .text {
+    font-family: Pretendard;
+    font-style: normal;
+    line-height: 140%; /* 25.2px */
+  }
+  .name {
+    color: var(--Neutral-500, #1f1f1f);
+
+    font-size: 18px;
+    font-weight: 700;
+  }
+  .edit {
+    color: var(--Neutral-300, #848484);
+    font-size: 16px;
+    font-weight: 600;
+  }
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none;
+
+  font-family: Pretendard;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 21px; /* 123.529% */
+  letter-spacing: -0.32px;
+`;
