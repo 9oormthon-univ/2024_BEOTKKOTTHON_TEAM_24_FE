5a2323d96da6b2f71a3382ae663bfc0f538bc532
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-11-11 23:14:23 +0900

    chore: GA4, hotjar 코드 작성

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import type { DocumentContext, DocumentInitialProps } from 'next/document';
 import Document from 'next/document';
+import Script from 'next/script';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -35,15 +36,60 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y0X2WED3Y2"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Y0X2WED3Y2');`,
+          }}
+        />
         <Head>
-          <meta name="theme-color" content="#ffffff" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="icon" href="/svg/favicon.svg" type="image/svg+xml" />
-          <link rel="mask-icon" href="/svg/favicon.svg" color="#87B3FF" />
-          <link rel="apple-touch-icon" href="/icons/icon-512x512.png" />
+          <Script
+            id="GoogleTagmanager"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PZ7ZWCRC');`,
+            }}
+          />
+          <Script
+            id="hotjar"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5198774,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+            }}
+          />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-PZ7ZWCRC"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
